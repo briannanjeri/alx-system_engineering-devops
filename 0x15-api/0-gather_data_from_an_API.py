bf6aff66bc6e6497bafa82e1862eb30c0e59dbf6
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-09-25 17:14:59 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,46 @@
-#!/usr/bin/python3
-"""
-place holder
-"""
+import requests
+import sys
 
+# Function to fetch employee TODO list progress
+def get_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
+
+    try:
+        # Fetch employee information
+        employee_response = requests.get(employee_url)
+        employee_data = employee_response.json()
+
+        if employee_response.status_code != 200:
+            print(f"Error: Employee with ID {employee_id} not found.")
+            return
+
+        # Fetch TODO list for the employee
+        todo_response = requests.get(todo_url)
+        todo_data = todo_response.json()
+
+        # Calculate progress
+        total_tasks = len(todo_data)
+        completed_tasks = sum(1 for task in todo_data if task['completed'])
+
+        # Display progress information
+        print(f"Employee {employee_data['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
+        print(f"{employee_data['name']}:", end=' ')
+
+        for task in todo_data:
+            if task['completed']:
+                print("\n\t", task['title'])
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-    import requests
-    from sys import argv
-    if len(argv) < 2:
-        exit()
-    todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
-        .format(argv[1]))
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={}"
-        .format(argv[1]))
-    name = name.json()
-    name = name[0]["name"]
-    todo = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
-    todo = todo.json()
-    todo = len(todo)
-    todos = todos.json()
-    todo_list = []
-
-    for x in todos:
-        todo_list.append("\t {}".format(x["title"]))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, len(todos), todo))
-    for y in todo_list:
-        print(y)
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Error: Employee ID must be an integer.")

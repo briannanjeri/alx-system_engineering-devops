52bd7fa67eb931e84478dbd2de7b64bd2468e477
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2024-08-13 08:49:30 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# triggered puppet fix (to find out why Apache is returning a 500 error).
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',

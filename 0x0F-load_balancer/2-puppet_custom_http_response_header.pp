2a3fa10221a6368f216a70a7dc1392f6b1e1bace
briannanjeri <briannanjeri2020@gmail.com>
2024-08-13 12:31:24 +0300

    modified load balancers files

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Welcome to nginx on LXD container web1!</title>
-<style>
-  body {
-      width: 35em;
-      margin: 0 auto;
-      font-family: Tahoma, Verdana, Arial, sans-serif;
-  }
-</style>
-</head>
-<body>
-<h1>Welcome to nginx on LXD container web1!</h1>
-<p>If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
